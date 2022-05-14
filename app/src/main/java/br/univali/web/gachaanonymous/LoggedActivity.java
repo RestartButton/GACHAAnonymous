cb9e7642fc212cff42307dd3c92bcf733438867f
Rogério Franchini Borges Júnior <rogeriofbjr@hotmail.com>
2022-05-14 16:11:37 -0300

    loggout
    adicionado o botão de loggout adicionada lógica para esconder e aparecer botões qnd na logged_ativity

--- app/src/main/java/br/univali/web/gachaanonymous/LoggedActivity.java
+++ app/src/main/java/br/univali/web/gachaanonymous/LoggedActivity.java
@@ -1,7 +1,9 @@
 package br.univali.web.gachaanonymous;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class LoggedActivity extends AppCompatActivity {
+    private Menu mOptionsMenu;
     private NavigationBarView.OnItemSelectedListener navListener =
             new NavigationBarView.OnItemSelectedListener() {
                 @Override
@@ -20,12 +23,15 @@ public class LoggedActivity extends AppCompatActivity {
                     switch(item.getItemId()) {
                         case R.id.nav_log_home:
                             selectedFragment = new HomeFragment();
+                            onChangeFragment(selectedFragment);
                             break;
                         case R.id.nav_log_chat:
                             selectedFragment = new ChatFragment();
+                            onChangeFragment(selectedFragment);
                             break;
                         case R.id.nav_log_perfil:
                             selectedFragment = new ProfileFragment();
+                            onChangeFragment(selectedFragment);
                             break;
                     }
 
@@ -44,9 +50,30 @@ public class LoggedActivity extends AppCompatActivity {
         bottomNav.setOnItemSelectedListener(navListener);
 
 
+
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_logged,
                 new HomeFragment()).commit();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu){
+        mOptionsMenu  = menu;
+        MenuItem logoutBtn = mOptionsMenu.getItem(R.id.nav_log_logout);
+        logoutBtn.setVisible(false);
 
+        return true;
+    }
+
+    private void onChangeFragment(Fragment cFrag){
+        MenuItem logoutBtn = mOptionsMenu.getItem(R.id.nav_log_logout);
+        MenuItem profileBtn = mOptionsMenu.getItem(R.id.nav_log_perfil);
+
+        logoutBtn.setVisible(false);
+        profileBtn.setVisible(true);
+
+        if(cFrag instanceof ProfileFragment){
+            logoutBtn.setVisible(true);
+            profileBtn.setVisible(false);
+        }
     }
 }
